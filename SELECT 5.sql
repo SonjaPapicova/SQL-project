58015bbdea360f0d626d44e595383e0dda81060e
Sonja Papičová <sonja.papicova@seznam.cz>
2024-06-04 21:29:46 +0200

    upload the SQL script that answers 5th question
    - contains information if GDP affect changes in wages and food prices over the years

---
+++ SELECT 5.sql
@@ -0,0 +1,14 @@
+-- 5.otázka: Má výška HDP vliv na změny ve mzdách a cenách potravin? 
+-- Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+
+SELECT
+    YEAR,
+    GDP,
+    ROUND(AVG(value), 2) AS avg_wages,
+    ROUND(AVG(price_value), 2) AS avg_food_prices,
+    ROUND((GDP - LAG(GDP) OVER (ORDER BY YEAR)) / LAG(GDP) OVER (ORDER BY YEAR) * 100, 2) AS GDP_growth_percent,
+    ROUND((AVG(value) - LAG(AVG(value)) OVER (ORDER BY YEAR)) / LAG(AVG(value)) OVER (ORDER BY YEAR) * 100, 2) AS wage_growth_percent,
+    ROUND((AVG(price_value) - LAG(AVG(price_value)) OVER (ORDER BY YEAR)) / LAG(AVG(price_value)) OVER (ORDER BY YEAR) * 100, 2) AS food_price_growth_percent
+FROM t_sonja_papicova_project_sql_primary_final_rev
+GROUP BY YEAR, 
+		 GDP;
